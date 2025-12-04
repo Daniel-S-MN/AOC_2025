d3ae2268c5bf96e8c6bac293683fcd7ed8820f9b
Daniel <danielschwen@gmail.com>
2025-12-03 20:40:27 -0600

    Day 1 part 1 complete

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,16 @@ plugins {
     kotlin("jvm") version "2.2.21"
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21)) // Use Java 21
+    }
+}
+
+kotlin {
+    jvmToolchain(21) // Match Kotlin to the same version
+}
+
 sourceSets {
     main {
         kotlin.srcDir("src")
--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,31 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var dial = 50
+        var zeroes = 0
+        input.forEach {
+            if (it[0] == 'L') {
+                dial = (dial - it.drop(1).toInt()) % (100)
+            } else {
+                dial = (dial + it.drop(1).toInt()) % (100)
+            }
+            if (dial == 0) zeroes++
+        }
+        return zeroes
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+//    fun part2(input: List<String>): Int {
+//        return input.size
+//    }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    // check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // val testInput = readInput("Day01_test")
+    // check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+    // part2(input).println()
 }
